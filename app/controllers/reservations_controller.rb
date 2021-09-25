b0a40fb73a5b35724df22c3546630aef3316af09
Sinan Chaush <sinansonmez@outlook.com>
2021-09-25 16:17:01 +0200

    reservation dates are fixed

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
-class ReservationsController < ApplicationController 
+class ReservationsController < ApplicationController
   def index
-# TODO: flats to be filtered based on search parameters entered by user
+    # TODO: reservation to be filtered based on search parameters entered by user
     @reservations = Reservation.all
   end
 
@@ -28,11 +28,14 @@ class ReservationsController < ApplicationController
   end
 
   private
-   def find_flat
-     @flat = Flat.find(params[:flat_id])
+
+  def find_flat
+    @flat = Flat.find(params[:flat_id])
   end
 
   def reservation_params
-    params.require(:reservation).permit(:begin_date, :end_date)
+    # TODO pass begin and end date from home search
+    { "begin_date" => "26-09-2021", "end_date" => "28-09-2021" }
+    # params.require(:reservation).permit(:begin_date, :end_date)
   end
 end
